5d04dfb262b7941546627c90f3ab149e18697fa7
Kumar <anandx.kumar@intel.com>
2021-10-05 12:28:25 +0530

    Initial Commit

---
+++ src/Components/Master.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Button from '@material-ui/core/Button'
+import MiniDrawer from './Material-ui/Drawer'
+class Master extends Component {
+    render() {
+        return (
+            <MiniDrawer></MiniDrawer>
+        )
+    }
+}
+
+export default Master
